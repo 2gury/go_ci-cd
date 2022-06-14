af2cf6c1aab9257b4da313cf12759420c90b5b77
2gury <54157924+2gury@users.noreply.github.com>
2022-06-14 11:32:03 +0300

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -12,7 +12,6 @@ func main() {
 	mx.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hi"))
 	}).Methods(http.MethodGet)
-
 	
 	log.Println("Start server at :8080 port")
 	http.ListenAndServe(":8080", mx)
